d558be8c6fe6b6db9345120f020a9eaede6a3f9a
Francis Li <mail@francisli.com>
2024-05-06 17:56:40 -0700

    [Closes #1] New block plug-in scaffold generated

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '555be11391b899a77cfb');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '848c43e2d1ee98adfaa8');
